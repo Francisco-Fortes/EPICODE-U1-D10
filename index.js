8b6a240272363b80eeab2dca870a7e37b1e4c30e
Francisco-Fortes <fortesfco@gmail.com>
2022-10-09 23:31:31 +0200

    Second commit

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ me.skills.pop();
 console.log(me);
 // JS Functions
 console.log(
-  "---------------------------------------------------------------JS FUNCTION------------------------------------------------"
+  "---------------------------------------------------------------JS FUNCTIONS-----------------------------------------------"
 );
 /* EXERCISE 1
 Write a function called dice; it should randomize an integer number between 1 and 6.
@@ -95,98 +95,151 @@ Write a function called dice; it should randomize an integer number between 1 an
 console.log(
   "------------------------------------------------EXERCISE 1---------------------------------"
 );
+function dice() {
+  return 1 + Math.floor(Math.random() * 6);
+}
+console.log(dice());
 
 /* EXERCISE 2
-
 Write a function called whoIsBigger which receives 2 numbers as parameters and returns the biggest one.
-
 */
-
-/* EXERCISE 3
-
-Write a function called splitMe which receives a string as a parameter and returns an array with every word in that string.
-
-Ex.: splitMe("I love coding") => returns ["I", "Love", "Coding"]
-
-*/
-
-/* EXERCISE 4
-
-Write a function called deleteOne which receives a string and a boolean as parameters.
-
-If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
-
-*/
-
-/* EXERCISE 5
-
-Write a function called onlyLetters which receives a string as a parameter and returns it removing all the digits.
-
-Ex.: onlyLetters("I have 4 dogs") => returns "I have dogs"
-
-*/
-
-/* EXERCISE 6
-
-Write a function called isThisAnEmail which receives a string as a parameter and returns true if the string is a valid email address.
-
-*/
-
-/* EXERCISE 7
-
-Write a function called whatDayIsIt that should return the current day of the week.
-
-*/
-
-/* EXERCISE 8
-
-Write a function called rollTheDices which receives a number as a parameter.
-
-It should invoke the dice() function defined in Ex1 the specified amount of times,
-
-and return an object containing a sum property holding the sum of all values extracted
-
-and a values array containing the single values of the dicerolls themselves.
-
-Example: RollTheDices(3) => returns {
-
-sum: 10
-
-values: [3, 3, 4]
-
+console.log(
+  "------------------------------------------------EXERCISE 2---------------------------------"
+);
+function whoIsBigger(firstParameter, secondParameter) {
+  if (firstParameter > secondParameter) {
+    console.log("First parameter (", firstParameter, ") won");
+  }
+  if (secondParameter > firstParameter) {
+    console.log("Second parameter (", secondParameter, ") won");
+  }
+  else (firstParameter === secondParameter) {
+    console.log("Parameters introduced are exactly the same");
+  }
 }
 
+/* EXERCISE 3
+Write a function called splitMe which receives a string as a parameter and returns an array with every word in that string.
+Ex.: splitMe("I love coding") => returns ["I", "Love", "Coding"]
 */
 
+console.log(
+  "--------------WIP-----------------EXERCISE 3---------------------------------"
+);
+
+function splitMe(sentence) {
+for(let index = 0; index<sentence.length; i++ ) {
+  const currentCharacter = sentence[index];
+  if currentCharacter === "t" {
+    break;
+  }
+}
+}
+//   //   for (let index = 0;)
+//   // }
+//   // const sente = "I am doing this on Saturday";
+//   console.log(splitMe.split(" "));
+// }
+
+// const sente = "I am doing this on Saturday";
+// console.log(splitMe.split(" "));
+// const oneString = "I am doing this on Saturday"
+// const splitMe = function spl(oneString) {
+//   for
+// }
+/* EXERCISE 4
+Write a function called deleteOne which receives a string and a boolean as parameters.
+If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
+*/
+console.log(
+  "------------------------------------------------EXERCISE 4---------------------------------"
+);
+
+/* EXERCISE 5
+Write a function called onlyLetters which receives a string as a parameter and returns it removing all the digits.
+Ex.: onlyLetters("I have 4 dogs") => returns "I have dogs"
+*/
+console.log(
+  "------------------------------------------------EXERCISE 5---------------------------------"
+);
+function onlyLetters(){
+
+
+
+}
+const onlyLetters = "My password contains four numbers '1234', can you see it?";
+console.log(onlyLetters.split(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+/* EXERCISE 6
+Write a function called isThisAnEmail which receives a string as a parameter and returns true if the string is a valid email address.
+*/
+console.log(
+  "------------------------------------------------EXERCISE 6---------------------------------"
+);
+
+/* EXERCISE 7
+Write a function called whatDayIsIt that should return the current day of the week.
+*/
+console.log(
+  "------------------------------------------------EXERCISE 7---------------------------------"
+);
+
+/* EXERCISE 8
+Write a function called rollTheDices which receives a number as a parameter.
+It should invoke the dice() function defined in Ex1 the specified amount of times,
+and return an object containing a sum property holding the sum of all values extracted
+and a values array containing the single values of the dicerolls themselves.
+Example: RollTheDices(3) => returns {
+sum: 10
+values: [3, 3, 4]
+}
+*/
+console.log(
+  "------------------------------------------------EXERCISE 8---------------------------------"
+);
+function rollTheDices(){
+
+}
 /* EXERCISE 9
-
 Write a function called howManyDays which receives a date as a parameter and returns the number of days passed since that date.
-
 */
+console.log(
+  "------------------------------------------------EXERCISE 9---------------------------------"
+);
 
 /* EXERCISE 10
-
 Write a function called isTodayMyBirthday which should return true if today’s your birthday, false otherwise.
-
 */
+console.log(
+  "------------------------------------------------EXERCISE 10---------------------------------"
+);
+function isTodayMyBirthday(day, month, year) {
+  if (day === 13 && month === 2 && year === 1990) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 // JS Arrays & Objects
+console.log(
+  "------------------------------------------JS Arrays & Objects--------------------------------"
+);
 
 // NOTE: the movies array used in some exercises is defined at the end of this file
-
 /* EXERCISE 11
-
 Write a function called deleteProp which receives an object and a string as parameters,
-
 and returns the given object after deleting its property named as the given string.
-
 */
+console.log(
+  "------------------------------------------------EXERCISE 11---------------------------------"
+);
 
 /* EXERCISE 12
-
 Write a function called oldestMovie which finds the oldest movie in the provided movies array.
-
 */
+function oldestMovie ()
+const oldest = Math.min(...movies.year)
+
 
 /* EXERCISE 13
 
@@ -199,6 +252,11 @@ Write a function called countMovies which returns the number of movies contained
 Write a function called onlyTheTitles which creates an array with just the titles of the movies contained in the provided movies array.
 
 */
+function onlyTheTitles ()
+
+
+
+
 
 /* EXERCISE 15
 
